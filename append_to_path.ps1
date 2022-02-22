a50dde67317f1a828023fdf634743ba817449e36
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-02-22 07:47:34 +0100

    Added script to append value to path and list path

---
+++ append_to_path.ps1
@@ -0,0 +1,14 @@
+﻿if ($args.count -lt 1) {
+    Write-Error 'Missing positional argument'
+    exit 1
+}
+
+$path_to_add = $args[0]
+
+$old_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+
+$new_path = $old_path + $path_to_add
+
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
+
+Write-Output 'Path has been updated. Restart the terminal session to apply the changes'
---
+++ list_path.ps1
@@ -0,0 +1,5 @@
+﻿$path_segments = ($env:path).split(';')
+
+Foreach ($segment in $path_segments) {
+    Write-Output $segment
+}
