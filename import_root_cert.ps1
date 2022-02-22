777c484c94be1384c780c84650d37082176a6d74
Henrik Hjalmarsson <Skogstomten@users.noreply.github.com>
2022-02-22 19:12:20 +0100

    Added script to import root cert

---
+++ import_root_cert.ps1
@@ -0,0 +1,15 @@
+﻿$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+$cert_path = Read-Host 'Certificate path'
+$cert_path = Resolve-Path $cert_path
+$cert_password = Read-Host 'Pass phrase'
+$cert.Import($cert_path, $cert_password, "Exportable,PersistKeySet")
+
+$store = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::Root,
+    "localmachine"
+)
+$store.open("MaxAllowed") 
+$store.add($cert) 
+$store.close()
+
+Write-Host 'Cert imported'
