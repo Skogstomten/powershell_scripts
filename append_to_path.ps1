531e5c10e069769cb918776fb2f2d519968cb03d
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2022-02-22 08:03:20 +0100

    Fixed bug in append_to_path script where ; was missing inbetween current path and new path segment

--- append_to_path.ps1
+++ append_to_path.ps1
@@ -7,7 +7,7 @@ $path_to_add = $args[0]
 
 $old_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 
-$new_path = $old_path + $path_to_add
+$new_path = $old_path + ';' + $path_to_add
 
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
 
